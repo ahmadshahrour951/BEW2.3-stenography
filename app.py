848e77b7cc887a688f1792c4ee3eff8e6c4e7a25
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2021-12-06 12:45:08 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+[Day 7] Assignment: Steganography
+    - Turn in on Gradescope (https://make.sc/bew2.3-gradescope)
+    # /Lessons/Steganography
+    - Lesson Plan: https://make-school-courses.github.io/BEW-2.3-Web-Security/
+Deliverables:
+    1. All TODOs in this file.
+    2. Decoded sample image with secret text revealed
+    3. Your own image encoded with hidden secret text!
+"""
+def decode_image(path_to_png):
+    """
+    Decode image from path
+    """
+    # Open the image using PIL:
+
+    # Separate the red channel from the rest of the image:
+
+    # Create a new PIL image with the same size as the encoded image:
+def encode_image(path_to_png, secret_message):
+    """
+    Encode secret text to image from path
+    """
